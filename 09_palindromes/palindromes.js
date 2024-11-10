f329af4014c7078a63fbe2aaef8b513ce9de056b
Augusto <danielaugustolopes44@gmail.com>
2024-11-10 03:32:15 -0300

    Exercises 8-11 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let strContent = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+  
+  let reversedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .reverse()
+    .join('')
 
+  return strContent === reversedStr;
 };
 
 // Do not edit below this line
